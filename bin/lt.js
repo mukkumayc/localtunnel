92bea312beef8503445a3101633096a885afcc9f
mukkumayc <sk9078@ya.ru>
2024-08-07 18:31:13 +0400

    Add basic authentication

--- bin/lt.js
+++ bin/lt.js
@@ -49,6 +49,9 @@ const { argv } = yargs
   .option('print-requests', {
     describe: 'Print basic request info',
   })
+  .option('auth', {
+    describe: 'Basic auth password',
+  })
   .require('port')
   .boolean('local-https')
   .boolean('allow-invalid-cert')
@@ -73,6 +76,7 @@ if (typeof argv.port !== 'number') {
     local_key: argv.localKey,
     local_ca: argv.localCa,
     allow_invalid_cert: argv.allowInvalidCert,
+    auth: argv.auth,
   }).catch(err => {
     throw err;
   });
